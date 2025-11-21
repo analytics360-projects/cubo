8371e715779fa81b47a858a9c4d4032e4f0d9154
Heber Estrada <84289475+heberestrada85@users.noreply.github.com>
2025-11-21 12:29:18 -0600

    Fix marshmallow version in superset.sh

--- superset.sh
+++ superset.sh
@@ -91,7 +91,7 @@ pip install "apache-superset>=4.0.0"
 print_status "Instalando dependencias adicionales..."
 pip install flask-cors redis celery
 pip install psycopg2-binary
-pip install "marshmallow>=3.19.0,<4.0.0"
+pip install "marshmallow=-3.26.1"
 pip install Pillow  # Para screenshots y thumbnails
 pip install "numpy>=1.24.0"  # Compatible con Python 3.12
 
